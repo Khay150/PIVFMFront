254ea945a16183f1a652d6482e96b5366994dfe2
Khay150 <fvanotti15@gmail.com>
2023-05-15 00:07:14 -0300

    .

--- ProyectoIntegrador_VanottiFrancoMatias/BackEnd3/src/main/java/com/portfolio/vanot/Repository/IPersonaRepository.java
+++ ProyectoIntegrador_VanottiFrancoMatias/BackEnd3/src/main/java/com/portfolio/vanot/Repository/IPersonaRepository.java
@@ -5,6 +5,7 @@
 package com.portfolio.vanot.Repository;
 
 import com.portfolio.vanot.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,9 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface IPersonaRepository extends JpaRepository <Persona, Long> {
+public interface IPersonaRepository extends JpaRepository <Persona, Integer> {
+    
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
     
 }
